a254f6f7dac38a7944430a57d6861e890259825a
Hafsa Hikal <161166356+HafsaTarek@users.noreply.github.com>
2026-06-24 19:25:26 +0300

    Delete client directory

--- client/src/components/common/Card.jsx
+++
@@ -1,9 +0,0 @@
-import styles from './Card.module.css';
-
-export default function Card({ children, className = '', as: Component = 'div', ...rest }) {
-  return (
-    <Component className={`${styles.card} ${className}`} {...rest}>
-      {children}
-    </Component>
-  );
-}
